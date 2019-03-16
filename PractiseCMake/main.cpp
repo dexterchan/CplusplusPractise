58ac53a98242868b9febdb34e9047f768853e5ea
dexterchan <dexterchan@gmail.com>
2019-03-16 19:39:15 +0800

    upgrade to C17

--- PractiseCMake/main.cpp
+++ PractiseCMake/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "include/Pet.h"
 #include <memory>
-
+#include "include/MoveConstructor.h"
 int main(int, char**) {
     std::cout << "Hello, world!\n";
     std::string n ="cat";
     std::unique_ptr<Pet> pet(new Pet(n));
     pet->eat();
+
+    Pet p(n);
+    p.sound();
+
+    testMoveConstructor();
 }
